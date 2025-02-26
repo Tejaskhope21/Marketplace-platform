2268152b07f45579827f42d198e6be9446ed2a91
YadneshUmredkar@75 <yadneshumredkar75@gmail.com>
2025-02-26 19:35:26 +0530

    Hero

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,16 @@
 import React from "react";
-<<<<<<< HEAD
-import Navbar from "./components/Navbar/Navbar";
-//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-const App = () => {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
-=======
 import Footer from "./components/Footer/Footer";
-import Navebar from "./components/Navebar/Navebar";
+import Navebar from "./components/Navbar/Navbar";
+import "./App.css";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return <>
   <Navebar/>
+  <Home/>
   <Footer/>
   </>;
->>>>>>> 7c64c060c330798dd14beebe0e367ef6f4fe4a9c
+
 };
 
 export default App;
