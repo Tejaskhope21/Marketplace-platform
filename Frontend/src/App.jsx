425d0c556d065c29f32bce9fa9a5b02c57bf63f9
Tejaskhope21 <tejaskhope600@gmail.com>
2025-02-26 19:54:11 +0530

    NA

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
 //import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Footer/>
-      
+      <Home />
+      <Footer />
     </>
   );
 };
