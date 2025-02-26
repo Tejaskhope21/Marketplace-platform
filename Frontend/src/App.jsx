9d52c0b33affaf96db18b543d9059e0b9dbee41d
Tejaskhope21 <tejaskhope600@gmail.com>
2025-02-26 15:49:22 +0530

    updates

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,16 @@
 import React from "react";
-<<<<<<< HEAD
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 //import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Footer/>
+      
     </>
   );
-=======
-import Footer from "./components/Footer/Footer";
-import Navebar from "./components/Navebar/Navebar";
-
-const App = () => {
-  return <>
-  <Navebar/>
-  <Footer/>
-  </>;
->>>>>>> 7c64c060c330798dd14beebe0e367ef6f4fe4a9c
 };
 
 export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,21 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-<<<<<<< HEAD
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
-=======
 
-createRoot(document.getElementById('root')).render(
- 
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  
-)
->>>>>>> 7c64c060c330798dd14beebe0e367ef6f4fe4a9c
+  </StrictMode>
+);
