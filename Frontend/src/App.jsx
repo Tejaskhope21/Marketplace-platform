2e8a4853e2e76f7e451568946749cf0fc9f148f1
YadneshUmredkar@75 <yadneshumredkar75@gmail.com>
2025-02-28 18:24:28 +0530

    navebar fixed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-// import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
 //import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Home />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import Category from '../../components/Category/Category'
-import Footer from '../../components/Footer/Footer'
 
 function Home() {
   return (
@@ -10,7 +9,7 @@ function Home() {
 
       <Hero />
       <Category />
-      <Footer/>
+     
     </div>
   );
 }
